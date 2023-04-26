615632eb4ebd8d9f6734cacf15a1f1a15f23c9ec
Mia-Oh <songmiaoh@gmail.com>
2023-04-26 20:02:54 +0900

    feat:Login 페이지-파일 생성 #3

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const SLayout = styled.main``;
+
+const Login = () => {
+  return (
+    <>
+      <SLayout></SLayout>
+    </>
+  );
+};
+
+export default Login;
