1910df6f83cb7b38bff21bb4afed868b68ff92d7
Mia-Oh <songmiaoh@gmail.com>
2023-04-26 20:05:55 +0900

    feat:전체 게시글 조회 페이지-PostList 파일 생성 #7

---
+++ src/pages/PostList.jsx
@@ -0,0 +1,11 @@
+import Loading from '../components/Loading';
+
+const PostList = () => {
+  return (
+    <>
+      <Loading />
+      <p>게시글 리스트 페이지</p>
+    </>
+  );
+};
+export default PostList;
