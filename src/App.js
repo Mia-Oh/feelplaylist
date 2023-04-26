5527fa9130fe5e529aaf18363ccc04da87f37c98
Mia-Oh <songmiaoh@gmail.com>
2023-04-26 20:08:56 +0900

    feat:App 수정-경로 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './variables.css';
-import Loading from './pages/Loading';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PostList from './pages/PostList';
+import Login from './pages/Login';
+import MakePost from './pages/MakePost';
+import PostDetail from './pages/PostDetail';
+import VideoResult from './pages/VideoResult';
 
 const App = () => {
   return (
     <>
-      <Loading />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PostList />} />
+          <Route path="/category" element={<PostList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<MakePost />} />
+          <Route path="/category/:postId" element={<PostDetail />} />
+          <Route path="/video" element={<VideoResult />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
